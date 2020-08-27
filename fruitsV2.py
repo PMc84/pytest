05205962a499d1e24ea2571d5a14c7ff02942f94
Peter McCourt <peter.mccourt@Peters-MacBook-Pro.local>
2020-08-27 19:40:16 +0100

    Swtiched from lists to set

---
+++ fruitsV2.py
@@ -0,0 +1,35 @@
+# Declare variables 
+LIST1 = {'Orange', 'Apple', 'Raspberry', 'Pear'}
+LIST2 = {'Strawberry', 'Apple', 'Pear', 'Peach'}
+SHAREDLIST = {}
+
+# loop to print the content of LIST1 on new lines
+print("List 1 Content")
+print("**************")
+for i in LIST1:
+    print(i)
+print("**************")
+print("")
+
+# loop to print the content of LIST2 on new lines
+print("List 2 Content")
+print("**************")
+for i in LIST2:
+    print(i)
+print("**************")
+print("")
+
+
+
+# compare LIST1 and LIST2 and return as SHAREDLIST
+SHAREDLIST = LIST1.intersection(LIST2)
+
+
+# loop to print the content of SHAREDLIST on new lines
+print("Cross over of both Lists")
+print("************************")
+for i in SHAREDLIST:
+    print(i)
+print("************************")
+
+
